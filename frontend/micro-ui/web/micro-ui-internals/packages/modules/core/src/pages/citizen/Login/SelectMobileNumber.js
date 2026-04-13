37e18c3ff8c759768444ea65dfcdd21bf04dde18
avinash-kv <avinash.verma@sparrowsoftech.net>
2026-04-13 16:15:30 +0530

    feat(citizen): improve registration UI and OTP flow with better UX

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Login/SelectMobileNumber.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Login/SelectMobileNumber.js
@@ -1,35 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { CardText, FormStep, CitizenConsentForm, Loader, CheckBox } from "@djb25/digit-ui-react-components";
-import { Link } from "react-router-dom";
+import { CitizenConsentForm, Loader, CheckBox } from "@djb25/digit-ui-react-components";
 
-const SelectMobileNumber = ({ t, onSelect, showRegisterLink, mobileNumber, onMobileChange, config, canSubmit }) => {
+const SelectMobileNumber = ({ t, handleMobileNumberSelect, mobileNumber, setMobileNumber }) => {
   const [isCheckBox, setIsCheckBox] = useState(false);
-  const [isCCFEnabled, setisCCFEnabled] = useState(false);
   const [mdmsConfig, setMdmsConfig] = useState("");
-  const [error, setError] = useState("");
+  const [countryCode, setCountryCode] = useState("+91");
+  const [isCCFEnabled, setisCCFEnabled] = useState(false);
   const { isLoading, data } = Digit.Hooks.useCustomMDMS(Digit.ULBService.getStateId(), "common-masters", [{ name: "CitizenConsentForm" }]);
-
-  function setTermsAndPolicyDetails(e) {
-    setIsCheckBox(e.target.checked);
-  }
-
-  const checkDisbaled = () => {
-    if (isCCFEnabled?.isCitizenConsentFormEnabled) {
-      return !(mobileNumber.length === 10 && canSubmit && isCheckBox);
-    } else {
-      return !(mobileNumber.length === 10 && canSubmit);
-    }
-  };
 
   useEffect(() => {
     if (data?.["common-masters"]?.CitizenConsentForm?.[0]?.isCitizenConsentFormEnabled) {
       setisCCFEnabled(data?.["common-masters"]?.CitizenConsentForm?.[0]);
     }
   }, [data]);
-
-  const onLinkClick = (e) => {
-    setMdmsConfig(e.target.id);
-  };
 
   const checkLabels = () => {
     return (
@@ -41,37 +24,21 @@ const SelectMobileNumber = ({ t, onSelect, showRegisterLink, mobileNumber, onMob
               {data?.link && (
                 <span
                   id={data?.linkId}
-                  onClick={(e) => {
-                    onLinkClick(e);
-                  }}
                   className="privacy-link"
+                  onClick={(e) => {
+                    setMdmsConfig(e.target.id);
+                  }}
                 >
                   {t(`${data?.link}`, "Privacy Policy")}
                 </span>
               )}
               {data?.linkPostfix && <span>{t(`${data?.linkPostfix}`, "")}</span>}
-              {index == isCCFEnabled?.checkBoxLabels?.length - 1 && t("LABEL", "")}
+              {index === isCCFEnabled?.checkBoxLabels?.length - 1 && t("LABEL", "")}
             </span>
           );
         })}
       </span>
     );
-  };
-
-  const validateMobileNumber = () => {
-    if (/^\d{0,10}$/.test(mobileNumber)) {
-      setError("");
-    }
-  };
-
-  const handleMobileChange = (e) => {
-    const value = e.target.value;
-    if (/^\d{0,10}$/.test(value) || value === "") {
-      onMobileChange(e);
-      validateMobileNumber();
-    } else {
-      setError(t("CORE_COMMON_PROFILE_MOBILE_NUMBER_INVALID"));
-    }
   };
 
   if (isLoading) return <Loader />;
@@ -80,56 +47,106 @@ const SelectMobileNumber = ({ t, onSelect, showRegisterLink, mobileNumber, onMob
     e.preventDefault();
     const data = await Digit.DigiLockerService.register({ module: "REGISTER" });
     const redirectUrl = data.redirectURL;
-    console.log("data", data);
     sessionStorage.setItem("code_verfier_register", data?.codeverifier);
     window.location.href = redirectUrl;
   };
 
+  const isValidIndianMobile = (num) => {
+    return /^[6-9]\d{9}$/.test(num);
+  };
+
+  const getErrorMessage = () => {
+    if (!mobileNumber) return "Enter mobile number";
+    if (mobileNumber.length < 10) return "Must be 10 digits";
+    if (!/^[6-9]/.test(mobileNumber)) return "Must start with 6-9";
+    if (!isCheckBox) return "Please Agree to Privacy Policy";
+    return "";
+  };
+
   return (
-    <div className="select-mobile-number-wrapper">
-      <FormStep
-        isDisabled={checkDisbaled()}
-        onSelect={onSelect}
-        config={config}
-        t={t}
-        componentInFront={<div className="prefix-container">{t("CORE_COMMON_COUNTRY_CODE_PREFIX", "+91")}</div>}
-        onChange={handleMobileChange}
-        value={mobileNumber}
-        cardStyle={{
-          display: "contents",
-        }}
-      >
-        {error && <p className="error-text">{error}</p>}
+    <form onSubmit={() => handleMobileNumberSelect()}>
+      <div className="registration__header">
+        <h1 className="registration__title">Step 1: Enter Mobile Number</h1>
+        <span className="registration__step-count">1 of 3</span>
+      </div>
 
-        {isCCFEnabled?.isCitizenConsentFormEnabled && (
-          <div className="consent-form-wrapper">
-            <CheckBox
-              label={checkLabels()}
-              value={isCheckBox}
-              checked={isCheckBox}
-              onChange={setTermsAndPolicyDetails}
-              styles={{ marginBottom: "20px" }}
-            />
+      <p className="registration__description">Enter your mobile number to receive OTP.</p>
 
-            <CitizenConsentForm
-              t={t}
-              isCheckBoxChecked={setTermsAndPolicyDetails}
-              labels={isCCFEnabled?.checkBoxLabels}
-              mdmsConfig={mdmsConfig}
-              setMdmsConfig={setMdmsConfig}
+      <div className="registration__field">
+        <label className="registration__label">Mobile Number</label>
+
+        <div className="registration__input-group">
+          <select className="registration__select" value={countryCode} onChange={(e) => setCountryCode(e.target.value)} disabled>
+            <option>+91</option>
+            <option>+1</option>
+          </select>
+          <div className="registration__input">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#90a1b9"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-phone-icon lucide-phone"
+            >
+              <path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384" />
+            </svg>
+            <input
+              className="phone"
+              value={mobileNumber}
+              onChange={(e) => {
+                let value = e.target.value.replace(/\D/g, ""); // remove non-digits
+                if (value.length > 10) value = value.slice(0, 10); // limit to 10
+                setMobileNumber(value);
+              }}
+              placeholder="Enter mobile number"
+              inputMode="numeric"
             />
           </div>
-        )}
-
-        <div className="or-separator-container">
-          <div className="or-text">{t("OR_CONTINUE_WITH", "OR CONTINUE WITH")}</div>
-          <button className="digilocker-btn" type="button" onClick={(e) => register(e)}>
-            <img src="https://meripehchaan.gov.in/assets/img/icon/digi.png" alt="DigiLocker" />
-            {t("CS_REGISTER_WITH_DIGILOCKER", "Register with DigiLocker")}
-          </button>
         </div>
-      </FormStep>
-    </div>
+        <p className="registration__hint">We will send a 6-digit code</p>
+      </div>
+
+      {isCCFEnabled?.isCitizenConsentFormEnabled && (
+        <div className="consent-form-wrapper">
+          <CheckBox
+            label={checkLabels()}
+            value={isCheckBox}
+            checked={isCheckBox}
+            onChange={(e) => setIsCheckBox(e.target.checked)}
+            styles={{ marginBottom: "20px" }}
+          />
+
+          <CitizenConsentForm
+            t={t}
+            isCheckBoxChecked={(e) => setIsCheckBox(e.target.checked)}
+            labels={isCCFEnabled?.checkBoxLabels}
+            mdmsConfig={mdmsConfig}
+            setMdmsConfig={setMdmsConfig}
+          />
+        </div>
+      )}
+
+      <div className="or-separator-container">
+        <div className="or-text">{t("OR_CONTINUE_WITH", "OR CONTINUE WITH")}</div>
+        <button className="digilocker-btn" type="button" onClick={(e) => register(e)}>
+          <img src="https://meripehchaan.gov.in/assets/img/icon/digi.png" alt="DigiLocker" />
+          {t("CS_REGISTER_WITH_DIGILOCKER", "Register with DigiLocker")}
+        </button>
+      </div>
+
+      <div className="btn-tooltip-wrapper">
+        <button className="registration__button" disabled={!isValidIndianMobile(mobileNumber) || !isCheckBox} type="submit">
+          Send OTP
+        </button>
+
+        {(!isValidIndianMobile(mobileNumber) || !isCheckBox) && <div className="btn-tooltip">{getErrorMessage()}</div>}
+      </div>
+    </form>
   );
 };
 
